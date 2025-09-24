340566c510a86c2f43d75dd0d8a2d438bb981f3e
wusaber33 <13287970+wusaber33@user.noreply.gitee.com>
2025-09-24 14:37:59 +0800

    replace on_event withlifespan

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from app.router.user import router as user_router
 from app.router.examination import router as exam_router
 from app.router.question import router as question_router
-
+from contextlib import asynccontextmanager
 from app.config import get_settings
 from app.db.base import Base
 from app.db.session import engine
@@ -13,21 +13,22 @@ from app.db.redis import init_redis, close_redis
 
 settings = get_settings()
 
-app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
-app.include_router(user_router)
-app.include_router(exam_router)
-app.include_router(question_router)
-@app.on_event("startup")
-async def on_startup() -> None:
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     # For demo/dev: create tables automatically. In production, use Alembic migrations.
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     # Init Redis
     await init_redis()
-
-@app.on_event("shutdown")
-async def on_shutdown() -> None:
+    yield
     await close_redis()
+
+app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG,lifespan=lifespan)
+app.include_router(user_router)
+app.include_router(exam_router)
+app.include_router(question_router)
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
